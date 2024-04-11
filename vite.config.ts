59be93989b4ea1daa9449c7b21199d1fa8bcabba
LavaC <lavac@foxmail.com>
2024-04-11 21:30:45 +0800

    chore: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import solidPlugin from "vite-plugin-solid";0
 
 
 export default defineConfig({
-  // plugins: [solidPlugin()],
-  ssr:true,
-  server: {
-    // port: 3000,
-    preset:'vercel'
-  },
-  // build: {
-  //   target: "esnext",
-  // },
+    server: {
+      preset:'vercel'
+    },
 });
