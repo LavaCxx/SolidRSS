17729d46c876d0379057dfc0cb785a5944abeddf
LavaC <lavac@foxmail.com>
2024-04-11 20:33:07 +0800

    del: outdate file

--- src/utils/modal/types.ts
+++
@@ -1,9 +0,0 @@
-export type ModalProps = {
-    title: string
-    content?: string
-    confirm?: () => void
-    cancel?: () => void
-    confirmText?: string
-    cancelText?: string,
-    resolve?: () => void
-}
